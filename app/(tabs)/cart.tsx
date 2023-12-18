2257a1af13fb8b364a33572c75119e6278c91f23
WinstonT <tandiw22@gmail.com>
2023-12-18 21:41:11 +0700

    add cart quantity selector

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -5,30 +5,80 @@ import Product from "../../types/Product";
 import Cart from "../../types/Cart";
 import { FlatList, ListRenderItem, StyleSheet } from "react-native";
 import CartItem from "../../components/CartItem";
+import { Text, View } from "../../components/Themed";
+import Colors from "../../constants/Colors";
+import { TouchableOpacity } from "react-native";
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: Colors.white,
+    justifyContent: "center",
+    marginBottom: 8,
+    paddingVertical: 12,
+  },
+  headerText: {
+    fontWeight: "700",
+    textAlign: "center",
+    fontSize: 16,
+  },
   list: {
+    paddingHorizontal: 12,
+  },
+  footer: {
+    backgroundColor: Colors.white,
+    borderTopStartRadius: 40,
+    borderTopEndRadius: 40,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  totalPriceText: {
+    fontWeight: "700",
+    fontSize: 12,
+  },
+  totalPriceAmount: {
+    fontWeight: "700",
+    fontSize: 20,
+    color: Colors.primary,
+  },
+  buyButton: {
+    backgroundColor: Colors.primary,
+    paddingVertical: 4,
+    paddingHorizontal: 16,
+    borderRadius: 12,
+  },
+  buyButtonLabel: {
+    color: Colors.white,
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  base: {
+    flex: 1,
+    paddingTop: 20,
     paddingHorizontal: 12,
   },
 });
 
 interface CartProduct {
+  quantity: number;
   productId: number;
 }
 
 export default function CartScreen() {
-  const [isLoading, setLoading] = useState(false);
   const [cart, setCart] = useState<Cart>();
   const [products, setProducts] = useState<Product[]>([]);
+
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     getCart();
   }, []);
 
   const getCart = async () => {
-    setLoading(true);
-
-    const response = await axios
+    const response: CartProduct[] = await axios
       .get(`${Constants.baseUrl}/carts/${Constants.userId}`)
       .then((res) => {
         setCart(res.data);
@@ -38,7 +88,6 @@ export default function CartScreen() {
 
     const data = await Promise.all(
       response.map(async ({ productId }: CartProduct) => {
-        console.log(productId);
         return axios
           .get(`${Constants.baseUrl}/products/${productId}`)
           .then((res) => {
@@ -48,18 +97,44 @@ export default function CartScreen() {
     );
 
     setProducts(data);
-    setLoading(false);
+
+    let initialTotalPrice = 0;
+    response.forEach(({ quantity }, index) => {
+      initialTotalPrice = initialTotalPrice + quantity * data[index].price;
+    });
+    setTotalPrice(initialTotalPrice);
   };
 
   const renderItem: ListRenderItem<Product> = ({ item: product, index }) => (
-    <CartItem index={index} cart={cart} product={product} />
+    <CartItem
+      index={index}
+      cart={cart}
+      product={product}
+      setTotalPrice={setTotalPrice}
+    />
   );
 
   return (
-    <FlatList
-      data={products}
-      renderItem={renderItem}
-      contentContainerStyle={styles.list}
-    />
+    <>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>My Cart</Text>
+      </View>
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        contentContainerStyle={styles.list}
+      />
+      <View style={styles.footer}>
+        <View>
+          <Text style={styles.totalPriceText}>Total</Text>
+          <Text style={styles.totalPriceAmount}>
+            {`$${totalPrice.toFixed(2)}`}
+          </Text>
+        </View>
+        <TouchableOpacity style={styles.buyButton}>
+          <Text style={styles.buyButtonLabel}>BUY</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { View, Text } from "./Themed";
-import { StyleSheet, Image } from "react-native";
+import {
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import Product from "../types/Product";
 import Cart from "../types/Cart";
 import Colors from "../constants/Colors";
 import moment from "moment";
+import { Feather } from "@expo/vector-icons";
 
 const styles = StyleSheet.create({
   container: {
@@ -43,12 +49,19 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   quantity: {
-    fontSize: 12,
-    color: Colors.grey,
+    fontWeight: "700",
   },
   price: {
     fontWeight: "700",
     color: Colors.primary,
+  },
+  quantitySelectContainer: {
+    flexDirection: "row",
+  },
+  quantitySelectButton: {
+    padding: 4,
+    borderRadius: 12,
+    backgroundColor: Colors.primary,
   },
 });
 
@@ -56,17 +69,40 @@ interface CartItemProps {
   index: number;
   cart?: Cart;
   product: Product;
+  setTotalPrice: (value: React.SetStateAction<number>) => void;
 }
 
-export default function CartItem({ index, cart, product }: CartItemProps) {
-  const { image, title } = product;
+export default function CartItem({
+  index,
+  cart,
+  product,
+  setTotalPrice,
+}: CartItemProps) {
+  const { image, title, price } = product;
 
   const formattedDate = moment(cart?.date).format("ddd, DD MMM yyyy");
-  const quantity = cart?.products[index].quantity;
-  const formattedPrice = parseFloat(product.price).toFixed(2);
-  const totalPrice = parseFloat(
-    String(Number(formattedPrice) * Number(cart?.products[index].quantity))
-  ).toFixed(2);
+  const formattedPrice = parseFloat(price).toFixed(2);
+
+  const [quantity, setQuantity] = useState(cart?.products[index].quantity ?? 1);
+
+  const decreaseQuantity = useCallback(() => {
+    setQuantity((value) => {
+      if (value === 1) {
+        ToastAndroid.show("Removed item from cart", ToastAndroid.SHORT);
+      }
+      return value - 1;
+    });
+    setTotalPrice((total) => total - parseFloat(price));
+  }, [setQuantity]);
+
+  const increaseQuantity = useCallback(() => {
+    setQuantity((value) => value + 1);
+    setTotalPrice((total) => total + parseFloat(price));
+  }, [setQuantity]);
+
+  if (quantity === 0) {
+    return <></>;
+  }
 
   return (
     <View style={styles.container}>
@@ -84,10 +120,22 @@ export default function CartItem({ index, cart, product }: CartItemProps) {
         </Text>
         <Text style={styles.date}>{`Last viewed on ${formattedDate}`}</Text>
         <View style={styles.priceContainer}>
-          <Text style={styles.quantity}>
-            {`${quantity} x $${formattedPrice}`}
-          </Text>
-          <Text style={styles.price}>{`$${totalPrice}`}</Text>
+          <Text style={styles.price}>{`$${formattedPrice}`}</Text>
+          <View style={styles.quantitySelectContainer}>
+            <TouchableOpacity
+              onPress={decreaseQuantity}
+              style={[styles.quantitySelectButton, { marginRight: 12 }]}
+            >
+              <Feather name="minus" color={Colors.white} />
+            </TouchableOpacity>
+            <Text style={styles.quantity}>{quantity}</Text>
+            <TouchableOpacity
+              onPress={increaseQuantity}
+              style={[styles.quantitySelectButton, { marginLeft: 12 }]}
+            >
+              <Feather name="plus" color={Colors.white} />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </View>
